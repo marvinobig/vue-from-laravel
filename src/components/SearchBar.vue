816ad6bc30a2dcae75c8fb17e3441681c9f5926b
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-15 17:01:52 +0200

    style search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,13 +1,13 @@
 <script setup>
-// import { useSearchStore } from '@/stores/search';
+import { useSearchStore } from '@/stores/search';
 
-// const searchStore = useSearchStore();
+const searchStore = useSearchStore();
 </script>
 
 <template>
-    <form class="flex justify-between gap-2 bg-gray-800 p-3 rounded-lg">
+    <form class="flex justify-between gap-2 rounded-lg">
         <label class="capitalize" for="search" hidden>search</label>
-        <input class="focus:outline-blue-300 bg-white px-4 py-2 rounded-lg text-lg w-full" type="search" id="search"
+        <input class="focus:outline-blue-300 bg-gray-800 text-white mb-6 p-4 rounded-lg text-lg w-full shadow-lg" type="search" id="search" placeholder="Search..."
             v-model="searchStore.search">
     </form>
 </template>
