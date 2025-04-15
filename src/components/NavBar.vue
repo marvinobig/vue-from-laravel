89ed8b5c6c121e7b7a0ffd3573c19f749c881ed9
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-15 15:54:42 +0200

    remove extra class

---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav class="p-6 shadow-lg  mx-auto max-w-[800px] rounded-lg bg-gray-800 text-white">
+        <div class="flex gap-2 justify-between items-center">
+            <RouterLink class="text-[24px] hover:text-blue-300" to="/">Home</RouterLink>
+            <img alt="Vue logo" class="max-w-[40px]" src="@/assets/logo.svg" />
+            <RouterLink class="text-[24px] hover:text-blue-300" to="/posts">Posts</RouterLink>
+        </div>
+    </nav>
+</template>
