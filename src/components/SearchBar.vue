21a448ee2db30e41803a533a1b1221c1ae856b3e
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-15 16:42:43 +0200

    create search bar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+// import { useSearchStore } from '@/stores/search';
+
+// const searchStore = useSearchStore();
+</script>
+
+<template>
+    <form class="flex justify-between gap-2 bg-gray-800 p-3 rounded-lg">
+        <label class="capitalize" for="search" hidden>search</label>
+        <input class="focus:outline-blue-300 bg-white px-4 py-2 rounded-lg text-lg w-full" type="search" id="search"
+            v-model="searchStore.search">
+    </form>
+</template>
