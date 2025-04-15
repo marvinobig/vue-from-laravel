791319f1446c179c2cf65a06ecb91bfb5759d1c2
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-15 16:06:08 +0200

    add sticky position

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-  <header class="p-3">
-    <NavBar/>
+  <header class="sticky top-0 p-3">
+    <NavBar />
   </header>
 
   <RouterView />
