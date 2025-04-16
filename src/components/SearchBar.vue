89c105be1a12916f6adfd3c666707dad35cd223b
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-16 12:18:03 +0200

    prevent submit

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -5,7 +5,7 @@ const searchStore = useSearchStore();
 </script>
 
 <template>
-    <form class="flex justify-between gap-2 rounded-lg">
+    <form class="flex justify-between gap-2 rounded-lg" @submit.prevent>
         <label class="capitalize" for="search" hidden>search</label>
         <input class="focus:outline-blue-300 bg-gray-800 text-white mb-6 p-4 rounded-lg text-lg w-full shadow-lg" type="search" id="search" placeholder="Search..."
             v-model="searchStore.search">
