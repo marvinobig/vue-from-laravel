a6555e93f6c2951e88ee954883eef39bd3f55ad1
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-17 09:27:39 +0200

    add home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,37 @@ import ContentContainer from '@/components/ContentContainer.vue';
 <template>
   <main>
     <ContentContainer>
-      <h1>Frontpage</h1>
+      <h1 class="my-8 text-3xl font-bold text-center text-gray-800">The Vue From WordPress</h1>
+
+      <section class="leading-9 text-center my-8 text-gray-800">
+        <h2 class="text-xl font-semibold mb-2">Rational</h2>
+        <p>
+          The best way to learn is by building real, hands-on projects. I'm using Vue, WordPress, and Docker
+          to create a custom blog frontend from scratch. This project helps me understand how modern tools
+          work together in a practical setup. It’s a unique, portfolio-worthy build that reflects real-world
+          development workflows.
+        </p>
+      </section>
+
+      <section class="leading-9 text-center my-8 text-gray-800">
+        <h2 class="text-xl font-semibold mb-2">Aim</h2>
+        <p>
+          Build a site using a dockerized headless WordPress backend with a Vue frontend to display content.
+          The goal is to gain practical experience with Vue, WordPress, and Docker in a real-world workflow.
+        </p>
+      </section>
+
+      <section class="leading-9 text-center my-8 text-gray-800">
+        <h2 class="text-xl font-semibold mb-2">Functional Requirements</h2>
+        <ul class="list-inside">
+          <li>Posts can be added through an admin</li>
+          <li>Users can view posts through a separate interface</li>
+        </ul>
+      </section>
+
+      <section class="leading-9 text-center my-8 text-gray-800">
+        <h4 class="text-md font-semibold mb-2">Developed by marvinobig</h4>
+      </section>
     </ContentContainer>
   </main>
 </template>
