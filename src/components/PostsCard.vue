666282c24c4947d2f14353b4cfc5dc4518f2b30c
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-17 09:31:04 +0200

    utilize router link

--- src/components/PostsCard.vue
+++ src/components/PostsCard.vue
@@ -9,9 +9,9 @@ const posts = [1, 2, 3, 4, 5, 6];
                 class="aspect-square object-cover shadow-md rounded-lg w-full sm:max-w-[300px] sm:h-[300px]" alt="post">
             <div class="flex flex-col justify-between p-3 rounded-lg bg-gray-800 text-white w-full shadow-md">
                 <div>
-                    <a class="underline hover:text-blue-300" href="post-link">
+                    <RouterLink class="underline hover:text-blue-300" to="/posts/dummy-post">
                         <h2 class="font-bold text-2xl mb-4 capitalize">post title</h2>
-                    </a>
+                    </RouterLink>
                     <p>post summary</p>
                 </div>
                 <p class="text-sm text-blue-300 capitalize">post date</p>
