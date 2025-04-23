ae1a81e2367505271222d167538d34b326647bd4
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-23 12:50:18 +0200

    finish vue from laravel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@ import ContentContainer from '@/components/ContentContainer.vue';
       <section class="leading-9 text-center my-8 text-gray-800">
         <h2 class="text-xl font-semibold mb-2">Rational</h2>
         <p>
-          The best way to learn is by building real, hands-on projects. I'm using Vue, WordPress, and Docker
+          The best way to learn is by building real, hands-on projects. I'm using Vue and laravel
           to create a custom blog frontend from scratch. This project helps me understand how modern tools
           work together in a practical setup. It’s a unique, portfolio-worthy build that reflects real-world
           development workflows.
@@ -21,15 +21,15 @@ import ContentContainer from '@/components/ContentContainer.vue';
       <section class="leading-9 text-center my-8 text-gray-800">
         <h2 class="text-xl font-semibold mb-2">Aim</h2>
         <p>
-          Build a site using a dockerized headless WordPress backend with a Vue frontend to display content.
-          The goal is to gain practical experience with Vue, WordPress, and Docker in a real-world workflow.
+          Build a site using a laravel backend with a Vue frontend to display content.
+          The goal is to gain practical experience with Vue and laravel in a real-world workflow.
         </p>
       </section>
 
       <section class="leading-9 text-center my-8 text-gray-800">
         <h2 class="text-xl font-semibold mb-2">Functional Requirements</h2>
         <ul class="list-inside">
-          <li>Posts can be added through an admin</li>
+          <li>Posts can be added through laravel backend</li>
           <li>Users can view posts through a separate interface</li>
         </ul>
       </section>
