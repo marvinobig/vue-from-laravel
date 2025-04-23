e951e864df13addb3a25203e153c147042bb1fcf
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-04-23 12:51:10 +0200

    fix typo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,12 +6,12 @@ import ContentContainer from '@/components/ContentContainer.vue';
 <template>
   <main>
     <ContentContainer>
-      <h1 class="my-8 text-3xl font-bold text-center text-gray-800">The Vue From WordPress</h1>
+      <h1 class="my-8 text-3xl font-bold text-center text-gray-800">The Vue From Laravel</h1>
 
       <section class="leading-9 text-center my-8 text-gray-800">
         <h2 class="text-xl font-semibold mb-2">Rational</h2>
         <p>
-          The best way to learn is by building real, hands-on projects. I'm using Vue and laravel
+          The best way to learn is by building real, hands-on projects. I'm using Vue and Laravel
           to create a custom blog frontend from scratch. This project helps me understand how modern tools
           work together in a practical setup. It’s a unique, portfolio-worthy build that reflects real-world
           development workflows.
@@ -22,14 +22,14 @@ import ContentContainer from '@/components/ContentContainer.vue';
         <h2 class="text-xl font-semibold mb-2">Aim</h2>
         <p>
           Build a site using a laravel backend with a Vue frontend to display content.
-          The goal is to gain practical experience with Vue and laravel in a real-world workflow.
+          The goal is to gain practical experience with Vue and Laravel in a real-world workflow.
         </p>
       </section>
 
       <section class="leading-9 text-center my-8 text-gray-800">
         <h2 class="text-xl font-semibold mb-2">Functional Requirements</h2>
         <ul class="list-inside">
-          <li>Posts can be added through laravel backend</li>
+          <li>Posts can be added through Laravel backend</li>
           <li>Users can view posts through a separate interface</li>
         </ul>
       </section>
